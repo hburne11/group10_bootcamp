f4c2f0defac388b4591bc00be8e5d612f6af9842
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-09-11 11:58:43 -0400

    adding backup file

--- script/dynamic.js
+++ script/dynamic.js
@@ -1,53 +1,65 @@
-
 (() => {
-const productName = document.querySelector(".teamInfo h3"); // name
-const productSub = document.querySelector(".teamInfo h4"); //members position
-const contents = document.querySelector(".detail"); // members detail
-const buttons = document.querySelectorAll(".bioSwitcher button"); //buttons
-const profile1 = document.querySelector("#profileVanilla"); //profile 1
-const profile2 = document.querySelector("#profileCitrus"); //profile 2
-const profile3 = document.querySelector("#profileMelon"); //profile 3
-const dynaTeam = [
-  [
-    "Vanilla Orange",
-    `Vanilla and Orange Flavor`,
-    `The sweetness of an orange paired with the smoothness of vanilla are a match made in flavor heaven. Bright and bold, there's simply no other combination like it. Try it today!`,
-  ],
+  const productName = document.querySelector(".teamInfo h3"); // name
+  const productSub = document.querySelector(".teamInfo h4"); //members position
+  const contents = document.querySelector(".detail"); // members detail
+  const buttons = document.querySelectorAll(".bioSwitcher button"); //buttons
+  const profile1 = document.querySelector("#profileOne"); //profile 1
+  const profile2 = document.querySelector("#profileTwo"); //profile 2
+  const profile3 = document.querySelector("#profileThree"); //profile 3
+  const profile4 = document.querySelector("#profilFour"); //profile 4
+  const dynaTeam = [
+    [
+      "Hayden Burnell",
+      `UX/UI Designer`,
+      `A small town guy with big city dreams, Hayden adds a blend of creativity and leadership to CODI. Only 22 years old, he has already conquered multiple challenges he has faced in his young life. He pursued studying Interactive Media Design at Fanshawe College in London, Ontario, where he discovered his dream of Front-End Development. Hayden likes to take simple ideas and enhance them into something unimaginable.`,
+    ],
 
-  [
-    "Blueberry Mellon",
-    `Blueberry and Melon Flavors`,
-    `The blueberry flavor comes through first, followed by the watermelon. The taste is authentic, and the first sip transports you to the awesome warmness of the tropical countries.`,
-  ],
+    [
+      "Hei Yan Chow (Joanna)",
+      `Product Designer`,
+      `Born and raised in Hong Kong, Joanna has a background in cultural management and art programme coordination. She has lived in London, attended Fanshawe College where she studied Interactive Media Design. She is currently a UX/UI designer at CODI.`,
+    ],
 
-  [
-    "Raspberry Citrus",
-    `Raspberry and Citrus Flavor`,
-    `A green, acid and sweet natural profile. It evokes sensations of fresh juicy fruit with slightly crunchy floral undertones blended with woody notes taste at the end.`,
-  ],
-];
+    [
+      "Jerome Fernandez",
+      `Web Developer`,
+      `Digital Imaging specialist from the Philippines, Because of his core skills, he has the huge advantage when it comes to  coding, video editing, 3d animation etc..
+      `,
+    ],
 
-function showBioInfo() {
-  arrayIndex = this.dataset.arrayref;
-  productName.innerText = dynaTeam[arrayIndex][0];
-  productSub.innerText = dynaTeam[arrayIndex][1];
-  contents.innerText = dynaTeam[arrayIndex][2];
-}
+    [
+      "Cristine Noronha",
+      `Graphic Designer`,
+      `Cristine is a graphic designer. Her greatest interest is solving problems in visual communication for people. She likes to study user experience and interface design. When she is not working she loves to draw, try new types of coffee and desserts..`,
+    ],
+  ];
 
-function changeImageProd1() {
-  document.getElementById("defaultImg").src = "images/dynamic-prod-1.jpg";
-}
+  function showBioInfo() {
+    arrayIndex = this.dataset.arrayref;
+    productName.innerText = dynaTeam[arrayIndex][0];
+    productSub.innerText = dynaTeam[arrayIndex][1];
+    contents.innerText = dynaTeam[arrayIndex][2];
+  }
 
-function changeImageProd2() {
-  document.getElementById("defaultImg").src = "images/dynamic-prod-2.jpg";
-}
+  function changeImageProd1() {
+    document.getElementById("defaultImg").src = "images/dynamic01.jpg";
+  }
 
-function changeImageProd3() {
-  document.getElementById("defaultImg").src = "images/dynamic-prod-3.jpg";
-}
+  function changeImageProd2() {
+    document.getElementById("defaultImg").src = "images/dynamic02.jpg";
+  }
 
-buttons.forEach((button) => button.addEventListener("click", showBioInfo));
-profile1.addEventListener("click", changeImageProd1);
-profile2.addEventListener("click", changeImageProd2);
-profile3.addEventListener("click", changeImageProd3);
+  function changeImageProd3() {
+    document.getElementById("defaultImg").src = "images/dynamic03.jpg";
+  }
+
+  function changeImageProd4() {
+    document.getElementById("defaultImg").src = "images/dynamic04.jpg";
+  }
+
+  buttons.forEach((button) => button.addEventListener("click", showBioInfo));
+  profile1.addEventListener("click", changeImageProd1);
+  profile2.addEventListener("click", changeImageProd2);
+  profile3.addEventListener("click", changeImageProd3);
+  profile4.addEventListener("click", changeImageProd4);
 })();
---
+++ script/gallery.js
@@ -0,0 +1,48 @@
+const heart1 = document.querySelector('.h1')
+const heart2 = document.querySelector('.h2')
+const heart3 = document.querySelector('.h3')
+const heart4 = document.querySelector('.h4')
+const heart5 = document.querySelector('.h5')
+const heart6 = document.querySelector('.h6')
+const heart7 = document.querySelector('.h7')
+const heart8 = document.querySelector('.h8')
+const heart9 = document.querySelector('.h9')
+
+heart1.addEventListener("click", () => {
+  heart1.classList.toggle("heart2");
+});
+
+heart2.addEventListener("click", () => {
+  heart2.classList.toggle("heart2");
+});
+
+heart3.addEventListener("click", () => {
+  heart3.classList.toggle("heart2");
+});
+
+
+heart4.addEventListener("click", () => {
+  heart4.classList.toggle("heart2");
+});
+
+heart5.addEventListener("click", () => {
+  heart5.classList.toggle("heart2");
+});
+
+heart6.addEventListener("click", () => {
+  heart6.classList.toggle("heart2");
+});
+
+heart7.addEventListener("click", () => {
+  heart7.classList.toggle("heart2");
+});
+
+heart8.addEventListener("click", () => {
+  heart8.classList.toggle("heart2");
+});
+
+heart9.addEventListener("click", () => {
+  heart9.classList.toggle("heart2");
+});
+
+
